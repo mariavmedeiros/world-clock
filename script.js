0db176b5b7f728e0ad2982e338c4c4b0a8a5ed1c
Maria Medeiros <maitevelori@hotmail.com>
2023-02-05 17:49:52 -0800

    added more js
    added moment link cdn, added real data

--- script.js
+++ script.js
@@ -3,4 +3,21 @@
 let losAngelesElement = document.querySelector("#losangeles");
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-losAngelesDateElement.innerHTML = moment().format("MMMM D, YYYY ");
+let losAngelesTime = moment().tz("America/Los_Angeles");
+
+losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY ");
+losAngelesTimeElement.innerHTML = losAngelesTime.format(
+  "h:mm [<small>]A[</small>]"
+);
+
+//São Paulo
+
+let saoPauloElement = document.querySelector("#saopaulo");
+let saoPauloDateElement = saoPauloElement.querySelector(".date");
+let saoPauloTimeElement = saoPauloElement.querySelector(".time");
+let saoPauloTime = moment().tz("America/Sao_Paulo");
+
+saoPauloDateElement.innerHTML = saoPauloTime.format(" MMMM Do, YYYY");
+saoPauloTimeElement.innerHTML = saoPauloTime.format(
+  "h:mm [<small>]A[</small>]"
+);
