44a727f9378f179f525b0f814f687a9ada97163d
Maria Medeiros <maitevelori@hotmail.com>
2023-02-07 10:05:16 -0800

    updating html and JS

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
 function updateTime() {
   //Los Angeles
   let losAngelesElement = document.querySelector("#losangeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY ");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY ");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   //São Paulo
 
   let saoPauloElement = document.querySelector("#saopaulo");
-  let saoPauloDateElement = saoPauloElement.querySelector(".date");
-  let saoPauloTimeElement = saoPauloElement.querySelector(".time");
-  let saoPauloTime = moment().tz("America/Sao_Paulo");
+  if (saoPauloElement) {
+    let saoPauloDateElement = saoPauloElement.querySelector(".date");
+    let saoPauloTimeElement = saoPauloElement.querySelector(".time");
+    let saoPauloTime = moment().tz("America/Sao_Paulo");
 
-  saoPauloDateElement.innerHTML = saoPauloTime.format(" MMMM Do, YYYY");
-  saoPauloTimeElement.innerHTML = saoPauloTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    saoPauloDateElement.innerHTML = saoPauloTime.format(" MMMM Do, YYYY");
+    saoPauloTimeElement.innerHTML = saoPauloTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
   let citytimeZone = event.target.value;
+  let cityName = citytimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(citytimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
   <div class="city">
         <div>
-          <h2>${citytimeZone}</h2>
+          <h2>${cityName}</h2>
           <div class="date">${cityTime.format(" MMMM Do, YYYY")}</div>
         </div>
         <div class="time"> ${cityTime.format(
